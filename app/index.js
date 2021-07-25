4b890f74faf0a031e716b7c881e6cb4d7a384c01
Niraj-Fonseka <fonseka.live@gmail.com>
2021-07-25 11:24:07 -0500

    adding build

---
+++ app/index.js
@@ -0,0 +1,55 @@
+import clock from "clock";
+import * as document from "document";
+import { preferences } from "user-settings";
+import * as util from "../common/utils";
+import { HeartRateSensor } from "heart-rate";
+import { BodyPresenceSensor } from "body-presence";
+import { today } from 'user-activity';
+// Update the clock every minute
+clock.granularity = "minutes";
+
+// Get a handle on the <text> element
+const myLabel = document.getElementById("myLabel");
+const heartRate = document.getElementById('hr');
+const steps = document.getElementById('steps');
+const cal = document.getElementById('cal');
+
+// Update the <text> element every tick with the current time
+clock.ontick = (evt) => {
+  let today = evt.date;
+  let hours = today.getHours();
+  if (preferences.clockDisplay === "12h") {
+    // 12h format
+    hours = hours % 12 || 12;
+  } else {
+    // 24h format
+    hours = util.zeroPad(hours);
+  }
+  let mins = util.zeroPad(today.getMinutes());
+  myLabel.text = `${hours}:${mins}`;
+}
+
+
+if (HeartRateSensor) {
+  const hrm = new HeartRateSensor({ frequency: 1 });
+  hrm.addEventListener("reading", () => {
+    heartRate.text = `${hrm.heartRate}`;
+  });
+  hrm.start();
+  
+  if (BodyPresenceSensor) {
+  const body = new BodyPresenceSensor();
+  body.addEventListener("reading", () => {
+    if (!body.present) {
+      hrm.stop();
+    } else {
+      hrm.start();
+    }
+  });
+  body.start();
+}
+}
+
+
+steps.text = `${(today.adjusted.distance / 1609.344).toFixed(2) } mi`
+cal.text = `${today.adjusted.calories} cal`
